1342cdf4b86070985845ce08c6111283a771ce32
Piotr Glowania <piotr.glowania@gmail.com>
2020-07-01 20:52:33 +0200

    HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Piotr");
+        System.out.println(38);
+        System.out.println('G');
+    }
+}
+
